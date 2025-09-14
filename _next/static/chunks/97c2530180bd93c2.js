aa79a8cc1a3af9b5677463db3e3922a10b751252
aMonteSl <a.montesl.2020@alumnos.urjc.es>
2025-09-14 19:44:54 +0200

    Updates

---
+++ _next/static/chunks/97c2530180bd93c2.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/9cfdf2c5eaa34784.js",
+  "static/chunks/turbopack-7067fed7476cddd0.js"
+])
---
+++ _next/static/fBM2Sp0dq4kRc0p9M14yf/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/97c2530180bd93c2.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
